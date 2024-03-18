b929f0e9c742528f5c938bfe4eea2207f185920f
Kiter <kiter.xie@hkmci.com>
2024-03-18 16:06:09 +0800

    chore:去掉一些代码

--- src/components/ThreeNative/ThreeDemo1/common/createDemo1Three.ts
+++ src/components/ThreeNative/ThreeDemo1/common/createDemo1Three.ts
@@ -1,6 +1,5 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { AMFLoader } from 'three/examples/jsm/loaders/AMFLoader.js'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 
 import * as GeometryUtils from 'three/examples/jsm/utils/GeometryUtils.js'
